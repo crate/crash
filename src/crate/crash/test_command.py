fb07176c5dbb55a4b6f7a22f2e4c93baeec6d4a4
Mathias Fussenegger <f.mathias@zignar.net>
2014-05-19 11:07:45 +0200

    fix py3 test failure

--- src/crate/crash/test_command.py
+++ src/crate/crash/test_command.py
@@ -1,32 +1,22 @@
 import sys
-import os
 from unittest import TestCase
-from six import StringIO
+from six import PY2, StringIO
 import tempfile
+from io import TextIOWrapper
+from mock import patch
 
 from .command import CrateCmd, main
-from contextlib import contextmanager
 
 
-@contextmanager
-def capture(command, *args, **kwargs):
-    stdout = StringIO()
-    orig_out, sys.stdout = sys.stdout, stdout
-    command(*args, **kwargs)
-    sys.stdout = orig_out
-    stdout.seek(0)
-    yield stdout.read()
-
-
-def mock_stdin(data):
-    "Setup a fake stdin file"
-    # need a real file for select() in command.get_stdin()
-    stdin = tempfile.TemporaryFile()
+def fake_stdin(data):
+    if PY2:
+        stdin = tempfile.TemporaryFile()
+    else:
+        stdin = TextIOWrapper(tempfile.TemporaryFile())
     stdin.write(data)
     stdin.flush()
     stdin.seek(0)
-    orig_in, sys.stdin = sys.stdin, stdin
-    return orig_in
+    return stdin
 
 
 class CommandTest(TestCase):
@@ -38,36 +28,37 @@ class CommandTest(TestCase):
                               "+------+------+",
                               "+------+------+\n"])
         command = CrateCmd()
-        with capture(command.pprint, [], ['name', 'name']) as output:
-            self.assertEqual(expected, output)
+        with patch('sys.stdout', new_callable=StringIO) as output:
+            command.pprint([], ['name', 'name'])
+            self.assertEqual(expected, output.getvalue())
 
+    @patch('sys.stdin', fake_stdin(u"select 'via-stdin' from sys.cluster"))
     def test_stdin_cmd(self):
         "Test passing in SQL via stdin"
-        stmt = "select 'via-stdin' from information_schema.tables limit 1"
-        orig_argv = sys.argv[:]
-        sys.argv = ['testcrash',
-                    '--hosts', self.crate_host]
-        orig_in = mock_stdin(stmt)
-        with capture(main) as output:
-            self.assert_(output.find('via-stdin') != -1)
-        sys.stdin.close()
-        sys.stdin = orig_in
-        sys.argv = orig_argv
+        try:
+            orig_argv = sys.argv[:]
+            sys.argv = ['testcrash', '--hosts', self.crate_host]
+            with patch('sys.stdout', new_callable=StringIO) as output:
+                main()
+                output = output.getvalue()
+                self.assertTrue('via-stdin' in output)
+        finally:
+            sys.argv = orig_argv
 
+    @patch('sys.stdin', fake_stdin(u"select 'via-stdin' from sys.cluster"))
     def test_cmd_precedence(self):
         """Test precedence of SQL passed in via -c vs. stdin
         SQL passed in via --command should take precedence
         over stdin
         """
-        stmt = "select '%s' from information_schema.tables limit 1"
-        orig_argv = sys.argv[:]
-        sys.argv = ['testcrash',
-                    "--command", stmt % 'via-command',
-                    '--hosts', self.crate_host]
-        orig_in = mock_stdin(stmt % 'via-stdin')
-        with capture(main) as output:
-            self.assert_(output.find('via-command') != -1)
-            self.assert_(output.find('via-stdin') == -1)
-        sys.stdin.close()
-        sys.stdin = orig_in
-        sys.argv = orig_argv
+        try:
+            stmt = u"select 'via-command' from information_schema.tables limit 1"
+            orig_argv = sys.argv[:]
+            sys.argv = ['testcrash', "--command", stmt, '--hosts', self.crate_host]
+            with patch('sys.stdout', new_callable=StringIO) as output:
+                main()
+                output = output.getvalue()
+                self.assertTrue('via-command' in output)
+                self.assertFalse('via-stdin' in output)
+        finally:
+            sys.argv = orig_argv

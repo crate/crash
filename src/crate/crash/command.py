crate/crash

commit 343945b6d66a95ddeab3573bc7a9e14c49da04fa

Support pasting of multiple statements

src/crate/crash/command.py
@@ -178,6 +178,28 @@ def noargs_command(fn):
     return inner_fn
 
 
+def _parse_statements(lines):
+    """Return a generator of statements
+
+    Args: A list of strings that can contain one or more statements.
+          Statements are separated using ';' at the end of a line
+          Everything after the last ';' will be treated as the last statement.
+
+    >>> list(_parse_statements(['select * from ', 't1;', 'select name']))
+    ['select * from t1', 'select name']
+    """
+    lines = (l.strip() for l in lines if l)
+    lines = (l for l in lines if not l.startswith('--'))
+    parts = []
+    for line in lines:
+        parts.append(line.rstrip(';'))
+        if line.endswith(';'):
+            yield ' '.join(parts)
+            parts[:] = []
+    if parts:
+        yield ' '.join(parts)
+
+
 class CrateCmd(object):
 
     def __init__(self,
@@ -236,22 +258,12 @@ class CrateCmd(object):
         self.output_writer.write(result)
 
     def process(self, text):
-        if text.lstrip().startswith('--'):
-            return
-        text = text.rstrip()
         if text.startswith('\\') and not self.lines:
             self._try_exec_cmd(text.lstrip('\\'))
-        elif text.endswith(';'):
-            line = text.rstrip(';')
-            if self.lines:
-                self.lines.append(line)
-                line = ' '.join(self.lines)
-                self._exec(line)
-                self.lines[:] = []
-            else:
-                self._exec(line)
-        elif text:
-            self.lines.append(text)
+        else:
+            for statement in _parse_statements(text.split('\n')):
+                self._exec(statement)
+            self.lines = []
 
     def exit(self):
         if self.lines:

src/crate/crash/tests.py
@@ -25,6 +25,7 @@ import os
 import unittest
 import doctest
 import zc.customdoctests
+from . import command
 from crate.testing.layer import CrateLayer
 from .command import CrateCmd
 from .printer import ColorPrinter, PrintWrapper
@@ -94,6 +95,7 @@ def test_suite():
     suite.addTest(s)
     CommandTest.layer = crate_layer
     CommandTest.crate_host = crate_host
+    suite.addTest(doctest.DocTestSuite(command))
     suite.addTest(unittest.makeSuite(CommandTest))
     suite.addTest(unittest.makeSuite(CommandLineArgumentsTest))
     suite.addTest(unittest.makeSuite(CommandUtilsTest))

commit 0fba0866032832957d34eac8e17e3e63df51a314

Fix stdin handling
Broke with 343945b6d66a95ddeab3573bc7a9e14c49da04fa

src/crate/crash/command.py
@@ -219,7 +219,6 @@ class CrateCmd(object):
         self.cursor = self.connection.cursor()
         self.output_writer = output_writer or OutputWriter(
             PrintWrapper(), is_tty)
-        self.lines = []
         self.exit_code = 0
         self.expanded_mode = False
         self.sys_info_cmd = SysInfoCommand(self)
@@ -257,18 +256,19 @@ class CrateCmd(object):
                         self.get_num_columns())
         self.output_writer.write(result)
 
+    def process_iterable(self, stdin):
+        for statement in _parse_statements(stdin):
+            self._exec(statement)
+
     def process(self, text):
-        if text.startswith('\\') and not self.lines:
+        if text.startswith('\\'):
             self._try_exec_cmd(text.lstrip('\\'))
         else:
             for statement in _parse_statements(text.split('\n')):
                 self._exec(statement)
-            self.lines = []
 
     def exit(self):
-        if self.lines:
-            self._exec(' '.join(self.lines))
-            self.lines[:] = []
+        pass
 
     @noargs_command
     def _show_tables(self, *args):
@@ -492,9 +492,8 @@ def main():
         cmd.process(args.command)
         done = True
     elif stdin_data:
-        for data in stdin_data:
-            cmd.process(data)
-            done = True
+        cmd.process_iterable(stdin_data)
+        done = True
     if not done:
         from .repl import loop
         loop(cmd, args.history)
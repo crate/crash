1b3f7654093aaa2d9b778da87a8dca4df4f150e9
Naomi Slater <nslater@tumbolia.org>
2019-10-28 13:35:13 +0100

    Update config module name

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-from crate.theme.rtd.conf.crash import *
+from crate.theme.rtd.conf.clients_crash import *
 
 site_url = 'https://crate.io/docs/clients/crash/en/latest/'
 extensions = ['sphinx_sitemap']

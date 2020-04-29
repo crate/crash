3f8418eea72c80c0a6288984dbc24273a1e0d8c2
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-04-29 17:11:35 +0200

    Config update for crate-docs-theme 0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-from crate.theme.rtd.conf.clients_crash import *
+from crate.theme.rtd.conf.crate_crash import *
 
-site_url = 'https://crate.io/docs/clients/crash/en/latest/'
+site_url = 'https://crate.io/docs/crate/crash/en/latest/'
 extensions = ['sphinx_sitemap']

58cf315ae2b1607944f95511497b973ace3512f0
Andreas Motl <andreas.motl@crate.io>
2023-11-23 00:18:27 +0100

    CI: Update to CrateDB 5.5.0

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -28,7 +28,7 @@ from tests import ftouch
 if sys.platform != "linux":
     raise SkipTest("Integration tests only supported on Linux")
 
-crate_version = os.getenv("CRATEDB_VERSION", "5.4.5")
+crate_version = os.getenv("CRATEDB_VERSION", "5.5.0")
 crate_http_port = 44209
 crate_transport_port = 44309
 crate_settings = {

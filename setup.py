0993c63da849220abee72bb2f53bbf1602447d26
Andreas Motl <andreas.motl@crate.io>
2024-02-08 03:52:05 +0100

    Update to crate>=0.35.2
    It is needed to accompany the recent improvements about `--timeout`.
    Fixes: float() argument must be a string or a real number, not 'Timeout'

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup
 requirements = [
     'colorama<1',
     'Pygments>=2.4,<3',
-    'crate>=0.30.0',
+    'crate>=0.35.2',
     'platformdirs<5',
     'prompt-toolkit>=3.0,<4',
     'tabulate>=0.9,<0.10',

ec8b7dd03378185624c86fced7d5b2e6e5cd3a3a
Andreas Motl <andreas.motl@crate.io>
2024-11-14 00:31:25 +0100

    Python: Fix "implicit namespace packages" migration
    ... by omitting `__init__.py` from `crate` namespace package altogether,
    see "PEP 420" [1] and "Package Discovery and Namespace Package » Finding
    namespace packages" [2].
    
    [1] https://peps.python.org/pep-0420/
    [2] https://setuptools.pypa.io/en/latest/userguide/package_discovery.html#namespace-packages

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import io
 import os
 import re
 
-from setuptools import setup
+from setuptools import find_namespace_packages, setup
 
 requirements = [
     'colorama<1',
@@ -67,7 +67,7 @@ setup(
     platforms=['any'],
     license='Apache License 2.0',
     keywords='cratedb db data client shell',
-    packages=['crate.crash'],
+    packages=find_namespace_packages("src"),
     package_dir={"": "src"},
     entry_points={
         'console_scripts': [

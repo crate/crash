9f268c91b23078d24148e00889b97065f38d572f
Andreas Motl <andreas.motl@crate.io>
2025-01-16 20:36:09 +0100

    Update to `crate-2.0.0`, which uses `orjson` for JSON marshalling
    https://github.com/ijl/orjson

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup
 requirements = [
     'colorama<1',
     'Pygments>=2.4,<3',
-    'crate>=1.0.0.dev2',
+    'crate>=2.0.0.dev5',
     'platformdirs<5',
     'prompt-toolkit>=3.0,<4',
     'tabulate>=0.9,<0.10',

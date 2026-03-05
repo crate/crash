2bc533ccae8de8ed5a4c8d8a240da033e06801ec
Andreas Motl <andreas.motl@crate.io>
2026-03-05 22:36:53 +0100

    Chore: Make link checker ignore `www.wikiwand.com`
    403 Client Error: Forbidden

--- docs/conf.py
+++ docs/conf.py
@@ -4,3 +4,8 @@ from crate.theme.rtd.conf.crate_crash import *
 html_context.update({
     "display_version": True,
 })
+
+linkcheck_ignore += [
+    # 403 Client Error: Forbidden
+    r"https://www.wikiwand.com/.*",
+]
